3838dd6b73c4928a415bba8a3f8a9a68ecf120db
Ahren Agatep <36459220+ahrenagatep@users.noreply.github.com>
2024-04-15 10:49:31 -0400

    added comments

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     /*
+    == TOPICS ==
     GUI
     exception-handling
     file I/O
     polymorphism
     composition
+
+    == IDEA ==
+    task manager
+    a flexible task scheduler capable of handling tasks of different types
+    (e.g., work tasks, personal tasks, reminders)
      */
     public static void main(String[] args) {
         System.out.println("test");
