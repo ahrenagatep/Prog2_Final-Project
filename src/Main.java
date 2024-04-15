4833f351a54ab17bc7e9ed9c6d6987ee1e3e72cd
Ahren Agatep <36459220+ahrenagatep@users.noreply.github.com>
2024-04-15 10:43:51 -0400

    added comments

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
+    /*
+    GUI
+    exception-handling
+    file I/O
+    polymorphism
+    composition
+     */
     public static void main(String[] args) {
         System.out.println("test");
     }
