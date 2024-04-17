df6b0f1a71bc8b8eaaddb7a96009cd167b7e5364
Raiyou04 <yraiyan@nyit.edu>
2024-04-17 11:45:05 -0400

    ok

--- src/Task.java
+++ src/Task.java
@@ -1,3 +1,24 @@
+
+class Work {
+ String Projects;
+ String meeting;
+
+}
+
+class School {
+ String subject;
+ String clubs;
+
+}
+
+class Personal{
+String chores;
+String errands;
+
+}
+
+
 public class Task {
+
 
 }
