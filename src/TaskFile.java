02f5ddb0875c2102162cc875f779359e20b39d10
Raiyou04 <yraiyan@nyit.edu>
2024-04-17 14:50:45 -0400

    ok

--- src/TaskFile.java
+++ src/TaskFile.java
@@ -4,17 +4,43 @@ public class TaskFile {
 class Task {
  int time;
  int date;
+
+ Work work;
+ School school;
+ Personal personal;
+
+ Task( int time, int date, Work work, School school, Personal personal){
+  this.time = time;
+  this.date = date;
+  this.work = work;
+  this.school = school;
+  this.personal = personal;
+ }
 }
 class Work {
- String Projects;
+ String projects;
  String meeting;
+
+ Work(String projects, String meeting) {
+     this.projects = projects;
+     this.meeting = meeting;
+ }
 }
 class School {
  String subject;
  String clubs;
 
+ School(String subject, String clubs){
+    this.subject = subject;
+    this.clubs = clubs;
+ }
 }
 class Personal{
 String chores;
 String errands;
+
+Personal(String chores, String errands){
+     this.chores = chores;
+     this.errands = errands;
+}
 }
