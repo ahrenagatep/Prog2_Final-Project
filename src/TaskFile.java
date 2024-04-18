e5e4a6a8851d3638c57b611f07078b5d5c802943
Ahren Agatep <36459220+ahrenagatep@users.noreply.github.com>
2024-04-18 12:02:13 -0400

    minor changes

--- src/TaskFile.java
+++ src/TaskFile.java
@@ -2,45 +2,40 @@ public class TaskFile {
  // what
 }
 class Task {
- int time;
- int date;
-
- Work work;
- School school;
- Personal personal;
-
- Task( int time, int date, Work work, School school, Personal personal){
-  this.time = time;
-  this.date = date;
-  this.work = work;
-  this.school = school;
-  this.personal = personal;
- }
+    int time;
+    int date;
+    Work work;
+    School school;
+    Personal personal;
+    Task(int time, int date, Work work, School school, Personal personal){
+        this.time = time;
+        this.date = date;
+        this.work = work;
+        this.school = school;
+        this.personal = personal;
+    }
 }
 class Work {
- String projects;
- String meeting;
-
- Work(String projects, String meeting) {
-     this.projects = projects;
-     this.meeting = meeting;
- }
+    String projects;
+    String meeting;
+    Work(String projects, String meeting) {
+        this.projects = projects;
+        this.meeting = meeting;
+    }
 }
 class School {
- String subject;
- String clubs;
-
- School(String subject, String clubs){
-    this.subject = subject;
-    this.clubs = clubs;
- }
+    String subject;
+    String clubs;
+    School(String subject, String clubs){
+        this.subject = subject;
+        this.clubs = clubs;
+    }
 }
 class Personal{
-String chores;
-String errands;
-
-Personal(String chores, String errands){
-     this.chores = chores;
-     this.errands = errands;
-}
+    String chores;
+    String errands;
+    Personal(String chores, String errands){
+        this.chores = chores;
+        this.errands = errands;
+    }
 }
